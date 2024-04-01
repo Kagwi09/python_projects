94d6166194f6b5cb94b7074f4b74524bce738b84
Kagwi09 <kagwimwangi.m@gmail.com>
2024-04-01 11:36:12 +0300

    My first project

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,7 +5,7 @@ import random
 user_prompt = input('Rock, paper, Scissors ? : ')
 list1 = ['Rock', 'Paper', 'Scissors']
 computer_prompt = random.choice(list1)
-print(computer_prompt)
+print(f'Computer : {computer_prompt}')
 str1 = "It's a draw !"
 str2 = 'Rock wins !'
 str3 = 'Paper wins !'
@@ -21,14 +21,13 @@ if user_prompt == 'Paper' or user_prompt == 'paper':
     if computer_prompt == 'Paper':
         print(f'{str1}')
     elif computer_prompt == 'Rock':
-         print(f'{str3}')
+        print(f'{str3}')
     else:
         print(f'{str4}')
 if user_prompt == 'Scissors' or user_prompt == 'scissors':
-    if  computer_prompt == 'Scissors':
+    if computer_prompt == 'Scissors':
         print(f'{str1}')
     elif computer_prompt == 'Rock':
-         print(f'{str2}')
+        print(f'{str2}')
     else:
         print(f'{str4}')
-

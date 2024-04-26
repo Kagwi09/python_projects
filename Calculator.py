c6b7a54faccc5d761f766162363769bf87d535fd
Kagwi09 <kagwimwangi.m@gmail.com>
2024-04-26 15:13:33 +0300

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -16,27 +16,27 @@ def calculator(num1, num2, sym):
 
 
 def choices():
-    number1 = int(input('Enter the first number: '))
+    number1 = float(input('Enter the first number: '))
     continue_operation = True
     while continue_operation:
         symbol = input('Enter the operation (+, -, *, /): ')
-        number2 = int(input('Enter the second number: '))
+        number2 = float(input('Enter the second number: '))
 
         result = calculator(number1, number2, symbol)
 
         choice = input('Do you want to continue? Input(y) to continue operation, (n) to start'
                        ' a new operation and (x) to exit? ')
         if choice.lower() == 'n':
-            print(f'The result is {result}')
+            print(f'The result is {round(result, 3)}')
             choices()
         elif choice.lower() == 'y':
             number1 = result
-            print(f'The result is {result}')
+            print(f'The result is {round(result, 3)}')
         elif choice.lower() == 'x':
-            print(f'The result is {result}')
+            print(f'The result is {round(result, 3)}')
             continue_operation = False
         else:
-            print(f'The result is {result}')
+            print(f'The result is {round(result, 3)}')
             print(f'Invalid input')
             choices()
             continue
